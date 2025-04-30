cf0a40063a3ffa93715dc478108d9fbe082846d5
Arthur24519 <art_khach04@mail.ru>
2025-04-30 12:19:18 +0700

    1

--- AutoServis/AutoServis/settings.py
+++ AutoServis/AutoServis/settings.py
@@ -79,9 +79,9 @@ WSGI_APPLICATION = 'AutoServis.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'Auto_Service'),
-        'USER': os.getenv('POSTGRES_USER', 'postgres'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', '123654789'),
+        'NAME': 'Auto_Service',
+        'USER': 'postgres',
+        'PASSWORD': '123654789',
         'HOST': 'localhost',
         'PORT': '5432',
     }
